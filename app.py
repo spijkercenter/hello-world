431d8edc2f1d5dc79301b278bf9a8d45b87f6ed3
Ivo <ip.spijkerman@avans.nl>
2022-03-08 20:44:21 +0100

    application app via waitress serve

--- app.py
+++ app.py
@@ -1,19 +1,15 @@
 import flask
 
 
-def main():
-    app = flask.Flask(__name__)
+application = flask.Flask(__name__)
 
-    @app.route("/")
-    def index():
-        return "I can echo your request, go to `/[something]` to see me work!"
+@application.route("/")
+def index():
+    return "I can echo your request, go to `/[something]` to see me work!"
 
-    @app.route("/<arg>")
-    def echo(arg):
-        return f"Echo: {arg}"
+@application.route("/<arg>")
+def echo(arg):
+    return f"Echo: {arg}"
 
-    app.run(host="0.0.0.0", port=8181)
-
-
-if __name__ == "__main__":
-    main()
+if (__name__ == "__main__"):
+    application.run(host="0.0.0.0", port=8181)
